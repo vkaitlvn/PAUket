84bcb038e4800ffa2a0641dd1f789d85f8d19ad9
kaitlvns <kaitlynbrooks5441@gmail.com>
2021-11-09 16:36:08 -0500

    test push

---
+++ testelements.js
@@ -0,0 +1,9 @@
+export const FooterWrap = styled.div`
+    padding: 20px 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 1100px;
+    margin: 0 auto;
+`;
