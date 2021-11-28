4b3d3520c05f535745addc1ce1280866b1155532
MichaelA0709 <93737173+MichaelA0709@users.noreply.github.com>
2021-11-28 16:35:48 -0500

    Add files via upload

---
+++ PAUket/pauketDB.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE pauketDB;
+USE pauketDB;
+/* Table to store user data */
+CREATE TABLE Users 
+(
+	userID INT,
+    userFname VARCHAR(50),
+    userLname VARCHAR(50),
+    userEmail VARCHAR(50),
+    Occupation VARCHAR(8), /* (Student/Faculty/Security) */
+    safety_level INT
+);
+/* Table to store location data */
+CREATE TABLE Location
+(
+	Latitude FLOAT,
+    Longitude FLOAT,
+    Altitude FLOAT,
+    created_at DATETIME,
+    userID INT
+);
